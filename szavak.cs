a4f59d2eb0dfcb9585e5e05ccee5ceb91a01d4d6
wolfb25 <fbalint0126@gmail.com>
2023-12-05 10:50:20 +0100

    Create szavak.cs

---
+++ szavak.cs
@@ -0,0 +1,19 @@
+using System;
+using System.Collections.Generic;
+using System.Configuration;
+using System.Data;
+using System.Linq;
+using System.Threading.Tasks;
+using System.Windows;
+
+namespace akasztoFa {
+
+    public class Szo {
+
+        string Text;
+        public Szo(string sor) {
+            string[] cuccok = sor.Split(';');
+                
+        }
+    }
+}
