eb59e946e8b60ce213eff74eb41b24d81077c8c5
wolfb25 <fbalint0126@gmail.com>
2023-12-11 14:01:47 +0100

    Update Page2.xaml.cs

--- Page2.xaml.cs
+++ Page2.xaml.cs
@@ -24,18 +24,26 @@ namespace akasztoFa
 		private char[] szo;
 		private List<char> kitalalt_char = new List<char>();
 		private List<char> rossz_char = new List<char>();
-
+		List<Label> labellList;
 		public Page2()
 		{
 			InitializeComponent();
 			jatek = new Jatek(Page1.jatekosNev, Page1.Mode, Page1.hibaSzam);
 			szo = jatek.ValasztottSzo.ToCharArray();
+			labellList = new List<Label> {
+				betu1, betu2, betu3, betu4, betu5,
+				betu6, betu7, betu8, betu9, betu10,
+				betu11, betu12, betu13
+			};
+			labellList.RemoveRange(szo.Length, labellList.Count);
+			labellList.TrimExcess();
+			labellList.ForEach(l => l.Visibility = Visibility.Visible);
 		}
 
 		private void Button_Click(object sender, RoutedEventArgs e)
 		{
 			SortChar(betutipp.Text.ToLower().First());
-
+			
 		}
 
 		private void SortChar(char ch) {
